4bcc50ab1c4d4f77adfccdd3638fdc9b92a63bef
wangjiankai <wangjiankai@unisound.com>
2020-12-22 23:57:11 +0800

    Oauth2.0 - client

--- springsecurity101-cloud-oauth2-client/src/main/java/com/example/springsecurity101cloudoauth2client/WebSecurityConfig.java
+++ springsecurity101-cloud-oauth2-client/src/main/java/com/example/springsecurity101cloudoauth2client/WebSecurityConfig.java
@@ -9,11 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @Order(200)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    /**
-     * /路径和/login路径允许访问，其它路径需要身份认证后才能访问
-     * @param http
-     * @throws Exception
-     */
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
